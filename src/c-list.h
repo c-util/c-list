19d91467c0f31daf00740ec78467e39b2b7a49e9
David Herrmann <dh.herrmann@gmail.com>
2016-10-14 14:23:17 +0200

    build: relicense as ASL-2.0
    Relicense as ASL-2.0 and drop LGPL.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/c-list.h
+++ src/c-list.h
@@ -1,15 +1,6 @@
 #pragma once
 
 /*
- * This file is part of c-list. See COPYING for details.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.
- */
-
-/*
  * Circular Double Linked List Implementation in Standard ISO-C11
  *
  * This implements a generic circular double linked list. List entries must
